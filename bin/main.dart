0bfeed385f2d829518363dfbd31719853ca60a12
jmercklen <mercklenjeremy@gmail.com>
2023-09-05 11:32:51 +0200

    Exo 4 fini

--- bin/main.dart
+++ bin/main.dart
@@ -1,8 +1,10 @@
 import 'package:tp1/tp1/exo1.dart' as tp1;
 import 'package:tp1/tp1/exo2.dart';
 import 'package:tp1/tp1/exo3.dart';
+import 'package:tp1/tp1/exo4.dart';
 
 void main(List<String> arguments) {
   exo2(arguments[0]);
   exo3(arguments[0]);
+  exo4(arguments[0]);
 }
--- lib/tp1/exo4.dart
+++ lib/tp1/exo4.dart
@@ -1,7 +1,33 @@
 import '../tools/FileReader.dart';
 
 countWords(FileReader fichier) {
-  int nbMots = 0;
+  if (!fichier.isEndOfFile()) {
+    int nbMots = 0;
 
-  return nbMots;
+    while (!fichier.isEndOfFile()) {
+      int nbCaracteres = 0;
+      String caractereActuel = fichier.readNext();
+
+      while (caractereActuel != " " &&
+          caractereActuel != "," &&
+          caractereActuel != "." &&
+          caractereActuel != ";" &&
+          caractereActuel != ":" &&
+          caractereActuel != "\n") {
+        nbCaracteres++;
+        if(!fichier.isEndOfFile())
+          caractereActuel = fichier.readNext();
+        else break;
+      }
+      if(nbCaracteres > 0)
+        nbMots++;
+    }
+    return nbMots;
+  } else
+    return 0;
+}
+
+exo4(String nomFichier) {
+  FileReader fichier = FileReader(nomFichier);
+  print(countWords(fichier));
 }
